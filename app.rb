44a039c81ebacdbbc3666629592e61bb38fe0d2e
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-05 15:16:04 -0600

    improve code

--- app.rb
+++ app.rb
@@ -28,25 +28,15 @@ get '/visit' do
 end
 
 post '/visit' do
-	# user_name, phone, date_time
-	@user_name = params[:user_name]
-	@phone = params[:phone]
-	@date_time = params[:date_time]
-	@barber = params[:barber]
-	#https://github.com/tkrotoff/jquery-simplecolorpicker
-	@color = params[:color]
-
 	#insert info into DB
 	Visit.find_or_create_by(
-		user_name: @user_name,
-		phone: @phone,
-		date_time: @date_time,
-		barber: @barber,
-		color: @color)
+		user_name: params[:user_name],
+		phone: params[:phone],
+		date_time: params[:date_time],
+		barber: params[:barber],
+		color: params[:color])
 
 	@title = "Thank you!"
-	#@message = "Dear #{@user_name}, your color is #{@color}, #{@barber} will wait for you on #{@date_time}"
-	
 	@message = Visit.all
 
 	erb :message
