d7a3e3b83a16a0dae209ebe071e1ba6a6abb353a
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-05 13:54:00 -0600

    added files for visit page

--- app.rb
+++ app.rb
@@ -11,8 +11,30 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.order "created_at DESC"
+end
+
 get '/' do
 	#@barbers = Barber.all
-	@barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	# user_name, phone, date_time
+	@user_name = params[:user_name]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+	#https://github.com/tkrotoff/jquery-simplecolorpicker
+	@color = params[:color]
+
+	@title = "Thank you!"
+	@message = "Dear #{@user_name}, your color is #{@color}, #{@barber} will wait for you on #{@date_time}"
+
+	erb :message
 end
