b794870d2267fb8ac668ad696b8ef073266da5e2
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-05 19:52:28 -0600

    more validation and fixed error with values

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
 	#validation of input (using method 'save'):
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
@@ -32,6 +32,7 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
